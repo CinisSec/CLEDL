d7bb28d438c9568f2a0b9eeb37374e13eea85759
grey <gregorypro@protonmail.com>
2023-03-29 18:10:56 +0200

    Initial commit

---
+++ CLEDL.c
@@ -0,0 +1,21 @@
+//LEDL port to C for study sake
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+
+//Checks if volume is available
+int checkVolume(char *path){
+    //DIR* dir = opendir(path);
+    printf("%s",path);
+    return 0;
+}
+
+int main(){
+    checkVolume("/Volumes/Data/Shared");
+
+    return 0;
+
+
+}
+
